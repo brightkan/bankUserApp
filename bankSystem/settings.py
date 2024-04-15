ccf19d844362a24361bb835bbdfdd0a62b37cc77
Bright Kanyange <40506019+brightkan@users.noreply.github.com>
2024-04-15 15:29:52 +0300

    Update settings.py

--- bankSystem/settings.py
+++ bankSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n_5qcl-2y1%!h^#!fakuujpux$8r90umrt0cr^ui(y=ihk&3%5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
